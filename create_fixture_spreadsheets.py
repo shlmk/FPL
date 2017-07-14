985568aba7e7b4a4fe6781887586ebcb73ad984c
shlmk <kingofsherlockspades@gmail.com>
2017-07-13 21:58:23 -0400

    Read team info from FPL site

--- create_fixture_spreadsheets.py
+++ create_fixture_spreadsheets.py
@@ -13,13 +13,22 @@ def get_data(fpl_link):
 if __name__ == "__main__":
   # https://stackoverflow.com/a/82852
   if not os.path.isfile('raw-data/fixtures.txt'):
-    data = get_data('https://fantasy.premierleague.com/drf/fixtures/')
+    data = json.loads(get_data('https://fantasy.premierleague.com/drf/fixtures/'))
     # https://stackoverflow.com/a/12309296
     with open('raw-data/fixtures.txt', 'w') as outfile:
       json.dump(data, outfile)
-  else:
-    #https://stackoverflow.com/a/2835672
-    with open('raw-data/fixtures.txt') as fixtures:
-      data = json.load(fixtures)
 
-    print(data)
+  if not os.path.isfile('raw-data/teams.txt'):
+    data = json.loads(get_data('https://fantasy.premierleague.com/drf/bootstrap-static'))
+
+    with open('raw-data/teams.txt', 'w') as outfile:
+      json.dump(data['teams'], outfile)
+  
+  #https://stackoverflow.com/a/2835672
+  with open('raw-data/fixtures.txt') as fixtures:
+    fixture_data = json.load(fixtures)
+
+  with open('raw-data/teams.txt') as teams:
+    team_data = json.load(teams)
+
+  print(team_data)
