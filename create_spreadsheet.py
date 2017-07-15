17fe759202e29ba4a6f00d4624d18c31a70ab9bc
shlmk <kingofsherlockspades@gmail.com>
2017-07-14 22:46:43 -0400

    Rename variable references elsewhere

--- create_spreadsheet.py
+++ create_spreadsheet.py
@@ -4,15 +4,15 @@ def create_spreadsheet(spreadsheet_name, data, desired_sheets='All'):
 
 def create_spreadsheet(spreadsheet_name, data, desired_sheets='all'):
 
-  if (desired_sheets!= 'All' and desired_sheets != 'opponents' and desired_sheets!= 'diffculty'):
+  if (desired_sheets!= 'all' and desired_sheets != 'opponents' and desired_sheets!= 'diffculty'):
     raise ValueError('Wrong value for wookbook -- must be ALL, Fixtures, or Diffculty!')
 
   workbook = xlsxwriter.Workbook(spreadsheet_name+'.xlsx')
 
-  if desired_sheets == 'ALL' or desired_sheets == 'Fixtures':
-    worksheet = workbook.add_worksheet('Fixtures')
+  if desired_sheets == 'all' or desired_sheets == 'opponents':
+    worksheet = workbook.add_worksheet('opponents')
 
-  if desired_sheets == 'ALL' or desired_sheets == 'Difficulty':
-    worksheet = workbook.add_worksheet('Diffculty')
+  if desired_sheets == 'all' or desired_sheets == 'difficulty':
+    worksheet = workbook.add_worksheet('difficulty')
 
   workbook.close()
